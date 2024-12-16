6218be02255cfadeb62493bafea87d7badf379e5
ZARRADIHEB <126171975+ZARRADIHEB@users.noreply.github.com>
2024-12-16 09:53:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -80,5 +80,5 @@ const getWeather = async () => {
 const date = new Date();
 const hours = date.getHours().toString().padStart(2, "0");
 const minutes = date.getMinutes().toString().padStart(2, "0");
-update.textContent = `  Last updated:
+update.textContent = `  Last update:
     ${hours}:${minutes}`;
